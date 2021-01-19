8d37b72e739c86ce1fca76f41633368e096575a1
YuRei Huang <yurei79601@gmail.com>
2021-01-20 01:48:31 +0800

    [new] add the new file: basic_notions.py for code quality

---
+++ code_quality/basic_notions.py
@@ -0,0 +1,28 @@
+"""
+This part should write down a simpy introduction to this file
+"""
+
+
+def my_function(var1:int, var2:int) -> int:
+    """
+    Brief description of the goal of this function
+
+    Args:
+        To give the statement or explain the meaning of arguments
+        - var1: explain meaning of var1
+        - var2: explain meaning of var2 ... 
+    
+    Returns:
+        To describe what would return after use of this function
+
+    Example:
+        my_function(1, 2) = 3
+    """
+    return var1 + var2
+
+if __name__ == '__main__':
+    print('the annotations of my_function is :')
+    print(my_function.__annotations__)
+    print('\n')
+    print('the docstring of my_function is:')
+    print(my_function.__doc__)
