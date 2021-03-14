68d4b677133eaa914aa00fa0e37da2f4c00ad4c9
YuRei Huang <yurei79601@gmail.com>
2021-03-14 16:14:06 +0800

    [feature] add n_channels in config.py to decribe channel for input data

--- tensorflow_notes/neural_networks/config.py
+++ tensorflow_notes/neural_networks/config.py
@@ -6,6 +6,8 @@ img_h = img_w = 28  # MNIST images are 28x28
 
 img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
 
+n_channels = 1
+
 h1 = 200  # number of nodes in the 1st hidden layer
 
 n_classes = 10  # Number of classes, one class per digit
