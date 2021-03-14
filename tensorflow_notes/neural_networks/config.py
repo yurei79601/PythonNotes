40ed122fc99af2339e290489c23fde98ef417719
YuRei Huang <yurei79601@gmail.com>
2021-03-14 14:13:52 +0800

    [refactor] add config settings

--- tensorflow_notes/neural_networks/config.py
+++ tensorflow_notes/neural_networks/config.py
@@ -1,3 +1,23 @@
+"""
+configuation setting for neural network model
+"""
+
 img_h = img_w = 28  # MNIST images are 28x28
+
 img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
+
+h1 = 200  # number of nodes in the 1st hidden layer
+
 n_classes = 10  # Number of classes, one class per digit
+
+epochs = 10  # Total number of training epochs
+
+batch_size = 64  # Training batch size
+
+display_freq = 100  # Frequency of displaying the training results
+
+learning_rate = 0.001  # The optimization initial learning rate
+
+model_path = './'
+
+data_path = 'MNIST_data'
