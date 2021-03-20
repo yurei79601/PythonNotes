eced71ec2f3d23aa72542fa8c8e05fb3dbfa5ea3
YuRei Huang <yurei79601@gmail.com>
2021-03-21 02:01:56 +0800

    [clean] delete useless variable in config.py

--- tensorflow_notes/neural_networks/config.py
+++ tensorflow_notes/neural_networks/config.py
@@ -4,8 +4,6 @@ configuation setting for neural network model
 
 img_h = img_w = 32  # MNIST images are 28x28
 
-img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
-
 n_channels = 3
 
 hidden_dimension_list = [512, 256, 128, 64] # number of nodes in hidden layers
@@ -15,8 +13,6 @@ n_classes = 10  # Number of classes, one class per digit
 epochs = 10  # Total number of training epochs
 
 batch_size = 64  # Training batch size
-
-display_freq = 100  # Frequency of displaying the training results
 
 learning_rate = 0.001  # The optimization initial learning rate
 
