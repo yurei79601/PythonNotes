b0e7aeead486a15e1ca2109c1b834dc7e08df768
YuRei Huang <yurei79601@gmail.com>
2021-03-28 01:22:29 +0800

    [feature] use data_description to replace information of data

--- tensorflow_notes/neural_networks/config.py
+++ tensorflow_notes/neural_networks/config.py
@@ -2,13 +2,7 @@
 configuation setting for neural network model
 """
 
-img_h = img_w = 32  # MNIST images are 28x28
-
-n_channels = 3
-
 hidden_dimension_list = [512, 256, 128, 64] # number of nodes in hidden layers
-
-n_classes = 10  # Number of classes, one class per digit
 
 epochs = 10  # Total number of training epochs
 
@@ -16,6 +10,25 @@ batch_size = 64  # Training batch size
 
 learning_rate = 0.001  # The optimization initial learning rate
 
-model_path = './'
+model_path = 'model_weights'
 
 data_path = 'MNIST_data'
+
+data_source = 'mnist'
+
+data_description = {
+    'mnist': {
+        'img_w': 28,
+        'img_h': 28,
+        'n_channels': 1,
+        'image_size_flat': 28 * 28,
+        'n_classes': 10
+    },
+    'cifar10': {
+        'img_w': 32,
+        'img_h': 32,
+        'n_channels': 3,
+        'image_size_flat': 32 * 32,
+        'n_classes': 10
+    }
+}
